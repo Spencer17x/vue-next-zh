af99e68f19e6da5426c4d57577ec6f46fd43d9b8
xuzepeng <xuzepeng@ingtube.com>
2020-04-30 20:05:27 +0800

    fix: 🐛 修改打包路径

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'Vue Composition API',
   description: 'Just playing around',
-  base: '/vue_next_zh/',
+  base: '.',
   themeConfig: {
     nav: [
       { text: 'RFC  ', link: '/' },
